40796420bd115a69463bccc06c6cded0b8de3038
xrdrsp <60511258+xrdrsp@users.noreply.github.com>
2020-03-20 09:27:46 +0800

    Update ding.cpp

--- ding.cpp
+++ ding.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main() {
     system("pause");
     SetCursorPos(1330, 840);
-    for (int i = 10000; ~i; i--) {
+    Sleep(2000);
+    for (int i=0;i<10000;i++)
+    {
         mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
     }
     return 0;
 }
-
-// Copyright By xrdrsp.
+// Copyright by xrdrsp and Dovuq (https://github.com/dovuq/).
