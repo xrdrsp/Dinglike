304614c465f8f59a284ba33f3a14b39ccd125b8e
xrdrsp <60511258+xrdrsp@users.noreply.github.com>
2020-04-10 10:15:11 +0800

    Update ding.cpp

--- ding.cpp
+++ ding.cpp
@@ -1,12 +1,21 @@
 #include <cstdlib>
+#include <cstdio>
+#include <iostream>
 #include <windows.h>
 using namespace std;
 int main() {
+    int x_coordinate, y_coordinate;
+    int num;
+    printf("Input your x-coordinate: ");
+    cin >> x_coordinate;
+    printf("Input your y-coordinate: ");
+    cin >> y_coordinate;
+    printf("Set the number of likes (No more than 100000 recommended): ");
+    cin >> num;
     system("pause");
-    SetCursorPos(1330, 840);
+    SetCursorPos(x_coordinate, y_coordinate);
     Sleep(2000);
-    for (int i=0;i<10000;i++)
-    {
+    for (int i = 0; i < num; i++) {
         mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
         mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
     }
